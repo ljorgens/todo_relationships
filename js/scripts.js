2d457e8c92ca04d3be7220b9c5bf74b0ff6d7456
Lee Jorgensen and Philip Orchard <student@epicodus.com>
2015-02-19 16:30:41 -0800

    Add functionality for second list

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,39 @@
 $(document).ready(function() {
+  var currentList;
   $("form#new-list").submit(function(event) {
     event.preventDefault();
 
     var list = $("input#list").val();
-    var newList = { listItem: list };
+    var newList = { listItem: list, choreList: [] };
 
-    $("#lists").append("<li>" + newList.listItem + "</li>");
+    $("#lists").append("<li class='list-name'>" + newList.listItem + "</li>");
     $("input#list").val("");
 
-    $("#lists li").last().click(function(event) {
-      $(this).appendTo("#completed");
-
-      $("#completed li").click(function(event){
-        $(this).remove();
+    $(".list-name").last().click(function() {
+      $("ul#choresToDO").text("");
+      currentList = newList;
+      currentList.choreList.forEach(function(chore) {
+        $("ul#choresToDO").append("<li>" + chore.newchore + "</li>");
       });
+      $("#lists_li").text(newList.listItem);
     });
+
   });
+    $("form#add_chore").submit(function(event) {
+      event.preventDefault();
+
+
+
+
+      $(".new-chores").each(function(){
+        var chore = $(this).find("input#newChoreName").val();
+        $("input#newChoreName").val("");
+        var newChore= { newchore: chore };
+        currentList.choreList.push(newChore);
+      });
+      $("ul#choresToDO").text("");
+        currentList.choreList.forEach(function(chore){
+          $("ul#choresToDO").append("<li>" + chore.newchore + "</li>");
+        });
+    });
 });
